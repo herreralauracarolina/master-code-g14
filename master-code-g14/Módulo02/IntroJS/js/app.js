8eea687b7c1fc288a77d8ee024170bc5fd8928e4
¨herreralauracarolina¨ <lcherrer@gmail.com>
2022-01-25 18:06:06 -0600

    Pendientes e intro a JS

---
+++ master-code-g14/Módulo02/IntroJS/js/app.js
@@ -0,0 +1,15 @@
+//Elementos paara mostrar la salida de datos
+//Muestra la salida en la consola
+console.log('You can do it, now exterior!')
+console.log(5+10)
+console.log(4==='4')
+
+//Muestra la salida en la pàgina
+document.write('You can do it, now exterior!')
+document.write(5+10)
+document.write(4==='4')
+
+//Muestra un pop-up con la salida de datos
+alert('You can do it, now exterior!')
+alert('5+10')
+alert(4==='4')
